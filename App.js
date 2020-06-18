36b8ae4e32af5034ba28147a25d65db616fd194b
afefhnid <afefhnid@gmail.com>
2020-06-19 00:44:44 +0200

    project init

---
+++ App.js
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import rootReducer from './helpers/rootReducer';
+import Splash from "./screens/Splash";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+import Profil from "./screens/Profil";
+import Search from "./screens/Search";
+const store = createStore(rootReducer);
+
+const BottomNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: Home,
+            navigationOptions: () => ({
+                tabBarIcon: ({tintColor}) => (
+                    <Icon
+                        name={"list"}
+                        color={tintColor}
+                        size={24}
+                    />
+                )
+            })
+        },
+        Search: {
+            screen: Search,
+            navigationOptions: () => ({
+                tabBarIcon: ({tintColor}) => (
+                    <Icon
+                        name={"search"}
+                        color={tintColor}
+                        size={24}
+                    />
+                )
+            })
+        },
+        Profil: {
+            screen: Profil,
+            navigationOptions: () => ({
+                tabBarIcon: ({tintColor}) => (
+                    <Icon
+                        name={"user"}
+                        color={tintColor}
+                        size={24}
+                    />
+                )
+            })
+        }
+
+    },
+    {
+        tabBarOptions: {
+            showLabel: false,
+            activeTintColor: '#7766C6',
+            inactiveTintColor: 'black',
+        }
+    }
+);
+
+const AppNavigator = createStackNavigator(
+    {
+        Splash: {screen: Splash, navigationOptions: {headerShown: false}},
+        Home: {screen: BottomNavigator, navigationOptions: {headerShown: false}},
+        Details: {screen: Details, navigationOptions: {headerShown: false}},
+    },
+    {
+        initialRouteName: 'Splash'
+    }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+class App extends Component{
+    render() {
+      return <Provider store={store}>
+          <AppContainer/>
+      </Provider>
+    }
+}
+
+export default App;
---
+++ screens/Home.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    FlatList
+} from 'react-native';
+import Title from "../components/Title";
+import EventBox from "../components/EventBox";
+import EventsService from "../services/events.service";
+
+class Home extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            eventsWeek: [],
+            eventsAfter: []
+        }
+    }
+
+    async componentDidMount() {
+        //let response = await EventsService.list();
+        let eventsWeek = await EventsService.getEventsThisWeek(10);
+        let eventsAfter = await EventsService.getEventsAfter();
+        this.setState({eventsWeek, eventsAfter});
+    }
+
+    render() {
+
+        let {eventsWeek, eventsAfter} = this.state;
+        let {navigation} = this.props;
+
+        return (
+            <ScrollView style={styles.container}>
+
+                <Title title={"Ce week-end"}/>
+
+                {/*{
+                    events.map(item => {
+                        return (<EventBox key={item.id}/>)
+                    })
+                }*/}
+
+                <FlatList
+                    data={eventsWeek}
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    backgroundColor={"#FFF"}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => <EventBox navigation={navigation} data={item.fields} horizontal={true}/>} />
+
+                <Title title={"A venir"}/>
+
+                <FlatList
+                    data={eventsAfter}
+                    showsHorizontalScrollIndicator={false}
+                    backgroundColor={"#FFF"}
+                    keyExtractor={item => item.id}
+                    renderItem={({item}) => <EventBox navigation={navigation} data={item.fields}/>} />
+
+
+            </ScrollView>
+        )
+    }
+
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF",
+        paddingTop: 70
+    }
+});
